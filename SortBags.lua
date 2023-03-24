c527f6bc81888b9931f927ead8ce952931c67e5d
ftkun <66640758+ftkun@users.noreply.github.com>
2023-03-24 17:39:30 +0300

    Update SortBags.lua
    increased deley, solved problem with grayed stuck items on many pservers

--- SortBags.lua
+++ SortBags.lua
@@ -157,7 +157,7 @@ do
 	f:SetScript('OnUpdate', function()
 		delay = delay - arg1
 		if delay <= 0 then
-			delay = .2
+			delay = .5
 
 			local complete = Sort()
 			if complete or GetTime() > timeout then
