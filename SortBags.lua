333054313701a7ab6ed361aff14363dd50f88538
ftkun <66640758+ftkun@users.noreply.github.com>
2023-04-10 17:00:05 +0300

    Update SortBags.lua

--- SortBags.lua
+++ SortBags.lua
@@ -157,7 +157,7 @@ do
 	f:SetScript('OnUpdate', function()
 		delay = delay - arg1
 		if delay <= 0 then
-			delay = .7
+			delay = 1.0
 
 			local complete = Sort()
 			if complete or GetTime() > timeout then
